2ad1265c7faa9760c971fda0e938f9b3c59b2abe
Mateusz Ostaszewski <me@mostaszewski.com>
2017-03-06 10:43:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='FolderCleaner',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/mostaszewski/FolderCleaner',
+    license='MIT License',
+    author='Mateusz Ostaszewski',
+    author_email='me@mostaszewski.com',
+    description='A simple script to sort and move files by their extension.'
+)
